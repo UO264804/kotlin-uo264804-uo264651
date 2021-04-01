ecea3279074b4b5247a1fc4cb7b9ce44dc364f41
UO264804 <UO264804@uniovi.es>
2021-04-01 20:07:45 +0200

    Primer intento de añadir a la lista elementos pero aun no funciona

---
+++ app/src/main/java/es/uniovi/appkotlin/AddHobbiesActivity.kt
@@ -0,0 +1,27 @@
+package es.uniovi.appkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import es.uniovi.appkotlin.databinding.ActivityAddHobbiesBinding
+
+
+class AddHobbiesActivity : AppCompatActivity()
+{
+    lateinit var binding: ActivityAddHobbiesBinding
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddHobbiesBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnAñadir.setOnClickListener {
+            val dat: String = binding.nombreHobbie.text.toString()
+            val returnIntent = Intent()
+            returnIntent.putExtra("nombreHobbie", dat)
+            setResult(RESULT_OK, returnIntent)
+            finish()
+        }
+    }
+}
